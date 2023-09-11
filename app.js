462f0a6ae9d243fa6ae1e16d7f674f01822099e7
marcin10lw <marcin10lw@wp.pl>
2023-09-11 21:24:57 +0200

    invoked express-async-errors

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
 require("dotenv").config();
+require("express-async-errors");
+
 const express = require("express");
 const app = express();
 const connectDB = require("./db/connect");
--- middleware/error-handler.js
+++ middleware/error-handler.js
@@ -1,5 +1,5 @@
 const errorHandleMiddleware = (err, req, res, next) => {
-  console.log(err);
+  console.log(err.message);
   res.status(500).json({ msg: "Something went wrong, please try again" });
 };
 
